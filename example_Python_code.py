057efd7ea58c1afda82b2ad6c99d13a3b2b5b619
Marcelo Alid <marcelo.alid@itau.cl>
2023-12-20 16:45:41 -0300

    first commit

---
+++ example_Python_code.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+d = {'col1': [1, 2], 
+     'col2': [3, 4]}
+
+df = pd.DataFrame(data = d)
+
+df
---
+++ example_R_code.R
@@ -0,0 +1,6 @@
+library(tibble)
+
+tbl <- tibble::tibble(col_1 = c(1, 2),
+                      col_2 = c(3, 4))
+
+tbl
